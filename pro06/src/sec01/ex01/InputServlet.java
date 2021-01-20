432a03e858461d4b642df94ef25bd66cc19797ce
dionysus0214 <hiba0214@naver.com>
2021-01-20 14:38:09 +0900

    자바 웹을 다루는 기술 6.4장 서블릿의 응답 처리 방법 진행중

--- pro06/src/sec01/ex01/InputServlet.java
+++ pro06/src/sec01/ex01/InputServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
 	public void init() throws ServletException {
 		System.out.println("init 메서드 호출");
 	}
@@ -17,8 +17,12 @@ public class LoginServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		String user_id = request.getParameter("user_id");
 		String user_pw = request.getParameter("user_pw");
-		System.out.println("아이디 :" + user_id);
+		System.out.println("아이디 : " + user_id);
 		System.out.println("비밀번호 : " + user_pw);
+		String[] subject = request.getParameterValues("subject");
+		for (String str : subject) {
+			System.out.println("선택한 과목 : " + str);
+		}
 	}
 
 	public void destroy() {
--- pro06/src/sec01/ex01/InputServlet2.java
+++ pro06/src/sec01/ex01/InputServlet2.java
@@ -1,24 +1,31 @@
 package sec01.ex01;
 
 import java.io.IOException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/input2")
+public class InputServlet2 extends HttpServlet {
 	public void init() throws ServletException {
 		System.out.println("init 메서드 호출");
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		String user_id = request.getParameter("user_id");
-		String user_pw = request.getParameter("user_pw");
-		System.out.println("아이디 :" + user_id);
-		System.out.println("비밀번호 : " + user_pw);
+		Enumeration enu = request.getParameterNames();
+		
+		while(enu.hasMoreElements()) {
+			String name = (String) enu.nextElement();
+			String[] values = request.getParameterValues(name);
+			for(String value : values) {
+				System.out.println("name : " + name + ", value : " + value);
+			}
+		}
 	}
 
 	public void destroy() {
